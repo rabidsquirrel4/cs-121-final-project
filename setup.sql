695f682f2fc19d3b519580d68660040fec1713ca
Rachel <shi.rachel.school@gmail.com>
2023-03-19 13:31:58 -0700

    deleted old setup.sql and replaced with modified version where NULL values are allowed

--- setup.sql
+++ setup.sql
@@ -9,8 +9,8 @@ DROP TABLE IF EXISTS bet_types;
 -- Defines the table that holds the client's information.
 CREATE TABLE client(
     client_id INT AUTO_INCREMENT,
-    username VARCHAR(25) UNIQUE NOT NULL,
-    password VARCHAR(25) NOT NULL,
+    -- Usernames are up to 20 characters, the same as in setup-passwords.sql
+    username VARCHAR(20) UNIQUE NOT NULL,
     is_admin BOOLEAN,
     PRIMARY KEY(client_id),
     CHECK (is_admin IN (0,1))
@@ -32,50 +32,66 @@ CREATE TABLE games(
     home_id INT NOT NULL, 
     away_id INT NOT NULL,
 
-    -- Goals scored by the home team at full time
-    ft_home_goals INT NOT NULL,
+    -- Goals scored by the home team at full time, may be NULL
+    ft_home_goals INT,
 
-    -- Goals scored by the away team at full time
-    ft_away_goals INT NOT NULL,
+    -- Goals scored by the away team at full time, may be NULL
+    ft_away_goals INT,
 
-    -- Goals scored by the home team at half time 
-    ht_home_goals INT NOT NULL,  
+    -- Goals scored by the home team at half time, may be NULL
+    ht_home_goals INT,  
 
-    -- Goals scored by the away team at half time 
-    ht_away_goals INT  NOT NULL,
+    -- Goals scored by the away team at half time, may be NULL 
+    ht_away_goals INT,
 
+    -- Shots taken by home team, may be NULL
+    home_shots INT,
 
-    home_shots INT NOT NULL,
-    away_shots INT NOT NULL,
+    -- Shots taken by away team, may be NULL
+    away_shots INT,
 
-    -- Fould committed by the hoome team
-    home_fouls_committed INT NOT NULL,
+    -- Fould committed by the home team, may be NULL
+    home_fouls_committed INT,
 
-    -- Fouls committed by the away team 
-    away_fouls_committed INT NOT NULL,
+    -- Fouls committed by the away team, may be NULL 
+    away_fouls_committed INT,
 
-    -- Corner kicks taken by the home team
-    home_corners INT NOT NULL,
+    -- Corner kicks taken by the home team, may be NULL
+    home_corners INT,
 
-    -- Corner kicks taken by the away team
-    away_corners INT NOT NULL,
+    -- Corner kicks taken by the away team, may be NULL
+    away_corners INT,
 
-    home_yellow_cards INT NOT NULL,
-    away_yellow_cards INT NOT NULL,
-    home_red_cards INT NOT NULL,
-    away_red_cards  INT NOT NULL,
+    -- Yellow cards given to home team, may be NULL
+    home_yellow_cards INT,
+    -- Yellow cards given to away team, may be NULL
+    away_yellow_cards INT,
+    -- Red cards given to home team, may be NULL
+    home_red_cards INT,
+    -- Red cards given to away team, may be NULL
+    away_red_cards INT,
 
     PRIMARY KEY (game_id),
     FOREIGN KEY(home_id) REFERENCES teams(team_id) ON UPDATE CASCADE  
                 ON DELETE CASCADE,
     FOREIGN KEY(away_id) REFERENCES teams(team_id) ON UPDATE CASCADE
                 ON DELETE CASCADE,
-    CHECK (ht_home_goals >= 0 AND ht_away_goals >= 0 AND ft_home_goals >= 0 AND
-            ft_away_goals >= 0),
-    CHECK ( home_fouls_committed >= 0 AND away_fouls_committed >= 0),
-    CHECK (home_yellow_cards >= 0 AND away_yellow_cards >= 0 AND 
-            home_red_cards >= 0 AND away_red_cards >= 0),
-    CHECK (home_corners >= 0 AND away_corners >= 0)
+
+    -- First check if one of the attributes that would be NULL if the game has
+    -- not happened yet is NULL (we only need to check one since if the game
+    -- has not happened yet, they should all be NULL)
+    -- If the attribute is NULL, the other statment in the OR statement should
+    -- not run, since we already know the whole statement is true
+    CHECK ((ht_home_goals IS NULL) OR
+            (ht_home_goals >= 0 AND ht_away_goals >= 0 AND ft_home_goals 
+            >= 0 AND ft_away_goals >= 0)),
+    CHECK ((home_fouls_committed IS NULL) OR 
+            (home_fouls_committed >= 0 AND away_fouls_committed >= 0)),
+    CHECK ((home_yellow_cards IS NULL) OR
+            (home_yellow_cards >= 0 AND away_yellow_cards >= 0 AND 
+            home_red_cards >= 0 AND away_red_cards >= 0)),
+    CHECK ((home_corners IS NULL) OR
+            (home_corners >= 0 AND away_corners >= 0))
 );
 
 
@@ -87,7 +103,7 @@ CREATE TABLE odds(
     draw NUMERIC(4,2) NOT NULL,
     away_win NUMERIC(4,2) NOT NULL,
     -- added DEFAULT value since one row in our imported data were missing these
-    -- values
+    -- values, temp fix
     `goals_over_2.5` NUMERIC(4,2) NOT NULL DEFAULT 0.0,
     `goals_under_2.5` NUMERIC(4,2) NOT NULL DEFAULT 0.0,
 
@@ -95,6 +111,7 @@ CREATE TABLE odds(
     FOREIGN KEY(game_id) REFERENCES games(game_id) ON UPDATE CASCADE 
                 ON DELETE CASCADE,
     CHECK (home_win > 0 AND draw > 0 AND away_win > 0),
+    -- temp fix
     CHECK (`goals_over_2.5` >= 0 AND `goals_under_2.5` >= 0)
 
 );
@@ -118,7 +135,9 @@ CREATE TABLE bets(
     client_id INT NOT NULL,
     type_id INT NOT NULL,
     amount_placed NUMERIC(10, 2) NOT NULL,
-    is_correct BOOLEAN NOT NULL,
+    -- whether the bet placed is correct or not, may be NULL if tne game hasn't 
+    -- hasn't already happened.
+    is_correct BOOLEAN, 
 
     PRIMARY KEY(bet_id),
     FOREIGN KEY(game_id) REFERENCES games(game_id) ON UPDATE CASCADE
@@ -127,6 +146,7 @@ CREATE TABLE bets(
                 ON DELETE CASCADE,
     FOREIGN KEY(type_id) REFERENCES bet_types(type_id) ON UPDATE CASCADE 
                 ON DELETE CASCADE,
-    CHECK (is_correct IN (0,1)),
+    -- check if is_correct is 1, 0, or NULL
+    CHECK ((is_correct IS NULL) OR (is_correct IN (0,1))),
     CHECK (amount_placed > 0.00)
 );
