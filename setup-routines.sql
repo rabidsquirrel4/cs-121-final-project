3b8adfb6015ec8de616e948d1f3ec2d643f13ca3
Rachel <shi.rachel.school@gmail.com>
2023-03-19 14:49:24 -0700

    decided on what functions/procedures/triggers to write

--- setup-routines.sql
+++ setup-routines.sql
@@ -8,10 +8,21 @@
 -- 1 Trigger
 
 DELIMITER !
--- A user-defined function that 
-CREATE FUNCTION
+-- A user-defined function that returns the result of a soccer match/game as a 
+-- string that can then be printed out
+CREATE FUNCTION show_outcome()
 BEGIN
+    IF ft_home_goals > ft_away_goals
 END !
+
+-- A user-defined function that calculates the earnings (or losses if negative)
+-- of a particular client as a numeric value with two decimal places.
+-- Returns 0.00 if client has not placed any bets.
+CREATE FUNCTION calc_earnings()
+BEGIN
+    
+END !
+
 DELIMITER ;
 
 
@@ -29,9 +40,19 @@ END !
 DELIMITER ;
 
 DELIMITER !
--- A trigger that
-CREATE TRIGGER trg_ AFTER INSERT
-       ON __ FOR EACH ROW
+-- A procedure that updates the is_correct boolean for bets after a particular 
+-- game has been decided.
+CREATE PROCEDURE sp_update_bets(
+    game_id INT,
+)
+BEGIN
+END !
+
+-- A trigger that updates the is_correct boolean for all bets after the game
+-- information in the games table has been updated for a particular game to 
+-- include non-NULL values for the information on how the game went.
+CREATE TRIGGER trg_update_outcomes AFTER UPDATE
+    ON games FOR EACH ROW
 BEGIN
     
 END !
